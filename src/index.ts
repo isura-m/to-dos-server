d4558d9e0c811e8e071854984d36d4415dcd7c37
isura <isura.gt1@hotmail.com>
2022-07-31 19:31:48 +0530

    add endpoint to create tasks

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,25 @@ import connectToDB from "./connectToDB";
 import TasksModel from "./models/TasksModel";
 
 const app = express();
+app.use(express.json());
 
 app.get("/to-dos", async (req: Request, res: Response) => {
   try {
     const tasks = await TasksModel.find();
     res.json({ success: true, tasks });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false });
+  }
+});
+
+app.post("/to-dos", async (req: Request, res: Response) => {
+  try {
+    const newTask = new TasksModel();
+    newTask.task = req.body.task;
+    newTask.notes = req.body.notes;
+    await newTask.save();
+    res.json({ success: true });
   } catch (err) {
     console.log(err);
     res.json({ success: false });
